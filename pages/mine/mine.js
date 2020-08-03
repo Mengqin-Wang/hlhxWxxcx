e0514b5b7992ca36c9a75de5d74b6063b6c59a5f
Mengqin-Wang <68188478+Mengqin-Wang@users.noreply.github.com>
2020-08-03 17:34:28 +0800

    Update mine.js

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -72,6 +72,16 @@ Page({
                                  
                                   // 可以传给后台，再经过解析获取用户的 openid
                                   // 或者可以直接使用微信的提供的接口直接获取 openid ，方法如下：
+                                //   wx.request({
+                                //       // 自行补上自己的 APPID 和 SECRET
+                                //     url: 'https://api.weixin.qq.com/sns/jscode2session?appid={{自己的appid}}&secret={{自己的secret}}&js_code=' + res.code + '&grant_type=authorization_code',
+                                //       success: res => {
+                                //           // 获取到用户的 openid
+                                //           console.log(res)
+                                //           console.log("用户的openid:" + res.data.openid)
+                                //           console.log("[session_key]", res.data.session_key)
+                                //       }
+                                //   });
 //                                   wx.request({
 //                                       // 自行补上自己的 APPID 和 SECRET
 //                                       url: 'https://api.weixin.qq.com/sns/jscode2session?appid={{自己的appid}}&secret={{自己的密码}}&js_code=' + res.code + '&grant_type=authorization_code',
@@ -82,6 +92,7 @@ Page({
 //                                           console.log("[session_key]", res.data.session_key)
 //                                       }
 //                                   });
+
                               }
                           });
                       }
